e270c1346e4a1a6f84b11f5a5dcb9dab14ed951e
Edcel Celiz <edceliz01@gmail.com>
2018-03-27 00:28:45 +0800

    edceliz-link v1

--- index.js
+++ index.js
@@ -3,19 +3,50 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const csrf = require('csurf')
 const path = require('path')
+const mongodb = require('mongodb')
+const crypto = require('crypto')
 const PORT = process.env.PORT || 5000
 
-express()
+const COLLECTION = 'edcelink'
+let db
+
+const app = express()
   .use(express.static(path.join(__dirname, 'public')))
   .use(bodyParser.urlencoded({ extended: false }))
   .use(cookieParser())
   .use(csrf({ cookie: true }))
+
+mongodb.MongoClient.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/edcelink', (err, client) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+  db = client.db()
+  console.log('MongoDB is ready')
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+})
+
+app
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => {
     res.render('pages/index', { token: req.csrfToken() })
   })
-  .post('/', (req, res) => {
-    res.render('pages/index', { token: req.csrfToken(), link: 'Heyyy' })
+  .get('/:code', (req, res) => {
+    db.collection(COLLECTION).findOne({ code: req.params.code}, (err, doc) => {
+      if (err || !doc)
+        res.render('pages/index', { token: req.csrfToken(), error: 'Link not found!' })
+      else
+        res.redirect(doc.url)
+    })
   })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .post('/', (req, res) => {
+    const url = req.body.url
+    const code = crypto.randomBytes(3).toString('hex')
+    db.collection(COLLECTION).insertOne({url, code}, (err, doc) => {
+      if (err)
+        res.render('pages/index', { token: req.csrfToken(), error: 'Server error! Please try again later.' })
+      else
+        res.render('pages/index', { token: req.csrfToken(), link: `${req.headers.host}/${code}` })
+    })
+  })
